5f1792a3f2c1d42112f3ca1dc8f8f3a2f0a6b32c
Sansan <sani.aliem@gmail.com>
2024-12-30 18:32:34 +0700

    test

--- resources/views/cardproduct.blade.php
+++ resources/views/cardproduct.blade.php
@@ -1,9 +1,9 @@
 <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 max-w-7xl mx-auto">
-    <x-card title="Nortwest Lake NE 02 25"
+    <x-card title="Nortwest Lakes NE 02 25"
         image="https://images.unsplash.com/photo-1631451095765-2c91616fc9e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
         location="Babat Jerawat, Kota Surabaya, Jawa Timur" status="Disewa" price="250 Juta/month" detailKM="4"
         detailKT="4" detailAreas="40000m2"></x-card>
-    <x-card title="Nortwest Lake NE 02 25"
+    <x-card title="Nortwest Lakes NE 02 25"
         image="https://images.unsplash.com/photo-1631451095765-2c91616fc9e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
         location="Babat Jerawat, Kota Surabaya, Jawa Timur" status="Disewa" price="250 Juta/month" detailKM="4"
         detailKT="4" detailAreas="40000m2"></x-card>
